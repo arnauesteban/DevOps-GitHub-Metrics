2e7c965873fe1d57a6b47f1c5f022f4fa5925cbc
Shodaisd <darkaxce@hotmail.com>
2023-10-04 17:01:31 -0400

    Update KanbanMetricsRouter.js

--- src/Back-end/routes/KanbanMetricsRouter.js
+++ src/Back-end/routes/KanbanMetricsRouter.js
@@ -7,7 +7,7 @@ class KanbanMetricsRouterRouter{
         this.init();
     }
 
-    async retrieveIssues(){
+    async retrieveIssues(req, res, next){
         let issues;
         let query = `
         query{
@@ -20,13 +20,13 @@ class KanbanMetricsRouterRouter{
                       ... on Issue {
                         title
                         state
+                        createdAt
+                        closedAt
                       }
                     }
                     status: fieldValueByName(name: "Status") {
                       ... on ProjectV2ItemFieldSingleSelectValue {
                         column: name
-                        updatedAt
-                        createdAt
                       }
                     }
                   }
@@ -53,6 +53,7 @@ class KanbanMetricsRouterRouter{
         await this.retrieveIssues().then((response)=>{issues = response});
         for(let i = 0; i< issues.length; i++){
             let issue = issues[i];
+            console.log(issue);
             for(let key in issue){
                 let column = "";
                 if(issue[key].column !== undefined){
@@ -63,14 +64,24 @@ class KanbanMetricsRouterRouter{
                 }
             }
         }
-        console.log(count)
+        console.log(count);
         return count;
+    }
+
+    async calculateNbIssueCompletedInTimeframe(req, res, next){
+      let count = 0
+      let columnName = req.params.columnName.replace(/[\u0300-\u036f]/g, '').toLowerCase().replaceAll("_"," ").replaceAll("\"", "");
+      
+      let issues;
+      await this.retrieveIssues().then((response)=>{issues = response});
     }
 
 
     init() {
         this.kanbanMetricsRouter.get('/log680/v1/issues/', this.retrieveIssues.bind(this));
         this.kanbanMetricsRouter.get('/log680/v1/nbIssues/:columnName', this.calculateNbIssuesPerColumn.bind(this));
+        this.kanbanMetricsRouter.post('/log680/v1/nbIssues/', this.calculateNbIssueCompletedInTimeframe.bind(this));
+        this.kanbanMetricsRouter.post('/log680/v1/nbIssues/', this.calculateNbIssueCompletedInTimeframe.bind(this));
     }
 }
 
