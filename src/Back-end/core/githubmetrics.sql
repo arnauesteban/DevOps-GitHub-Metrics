f1f9cddc16ec32f2a0bc8f8256057baf5773b3d4
Shodaisd <darkaxce@hotmail.com>
2023-10-27 10:03:08 -0400

    update bd

--- src/Back-end/core/githubmetrics.sql
+++ src/Back-end/core/githubmetrics.sql
@@ -20,13 +20,41 @@ DROP DATABASE IF EXISTS `githubmetrics`;
 CREATE DATABASE IF NOT EXISTS `githubmetrics` /*!40100 DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci */;
 USE `githubmetrics`;
 
--- Listage de la structure de table githubmetrics. githubmetricsdata
-DROP TABLE IF EXISTS `githubmetricsdata`;
-CREATE TABLE IF NOT EXISTS `githubmetricsdata` (
-  `Colonne 1` int(11) DEFAULT NULL
+-- Listage de la structure de table githubmetrics. metricsissueslt
+DROP TABLE IF EXISTS `metricsissueslt`;
+CREATE TABLE IF NOT EXISTS `metricsissueslt` (
+  `issueID` int(11) DEFAULT NULL,
+  `TimeStamp` int(11) DEFAULT NULL,
+  `Description` int(11) DEFAULT NULL,
+  `LeadTime` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Listage des données de la table githubmetrics.githubmetricsdata : ~0 rows (environ)
+-- Listage des données de la table githubmetrics.metricsissueslt : ~0 rows (environ)
+
+-- Listage de la structure de table githubmetrics. metricsltpr
+DROP TABLE IF EXISTS `metricsltpr`;
+CREATE TABLE IF NOT EXISTS `metricsltpr` (
+  `PullRequestID` int(11) DEFAULT NULL,
+  `TimeStamp` datetime DEFAULT NULL,
+  `Description` int(11) DEFAULT NULL,
+  `LeadTime` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Listage des données de la table githubmetrics.metricsltpr : ~0 rows (environ)
+
+-- Listage de la structure de table githubmetrics. metricssnapshot
+DROP TABLE IF EXISTS `metricssnapshot`;
+CREATE TABLE IF NOT EXISTS `metricssnapshot` (
+  `ProjectID` int(11) DEFAULT NULL,
+  `Timestamp` datetime DEFAULT NULL,
+  `Backlog` int(11) DEFAULT NULL,
+  `A_Faire` int(11) DEFAULT NULL,
+  `En_Cours` int(11) DEFAULT NULL,
+  `Revue` int(11) DEFAULT NULL,
+  `Complété` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Listage des données de la table githubmetrics.metricssnapshot : ~0 rows (environ)
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
